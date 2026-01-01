4a9c3ffac044d6bae40e464bbddf81869d0145f1
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2026-01-01 10:42:04 +0900

    add factorial

--- internal/gettingstarted/gettingstarted.go
+++ internal/gettingstarted/gettingstarted.go
@@ -9,7 +9,18 @@ func abs(n int) int {
 	return n
 }
 
-func formatAbs(x int) string {
+func FormatAbs(x int) string {
 	msg := "The absolute value of %d is %d"
 	return fmt.Sprintf(msg, x, abs(x))
+}
+
+func goFunc(n, acc int) int {
+	if n <= 0 {
+		return acc
+	}
+	return goFunc(n-1, n*acc)
+}
+
+func Factorial(n int) int {
+	return goFunc(n, 1)
 }
--- internal/gettingstarted/gettingstarted_test.go
+++ internal/gettingstarted/gettingstarted_test.go
@@ -3,16 +3,49 @@ package gettingstarted
 import (
 	"testing"
 
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_formatAbs(t *testing.T) {
+func Test_FormatAbs(t *testing.T) {
 	// given
 	in := -42
 
 	// when
-	actual := formatAbs(in)
+	actual := FormatAbs(in)
 
 	// then
 	assert.Equal(t, "The absolute value of -42 is 42", actual)
+}
+
+func Test_Factorial(t *testing.T) {
+	// given
+	in := 5
+
+	// when
+	actual := Factorial(in)
+
+	// then
+	assert.Equal(t, 120, actual)
+}
+
+func Test_FactorialProperties(t *testing.T) {
+	params := gopter.DefaultTestParameters()
+	params.MinSuccessfulTests = 100
+	properties := gopter.NewProperties(params)
+
+	properties.Property("Factorial(n) matches the product 1..n", prop.ForAll(
+		func(n int) bool {
+			expected := 1
+			for i := 1; i <= n; i++ {
+				expected *= i
+			}
+			return Factorial(n) == expected
+		},
+		gen.IntRange(0, 10),
+	))
+
+	properties.TestingRun(t)
 }
